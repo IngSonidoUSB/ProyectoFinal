d5ddb77fb85b9351cc488fa370fbe1c1a28efa17
maolivares10 <miguelolivares@MacBook-Air-de-Miguel.local>
2015-10-06 16:11:32 -0500

    Actualización Menu
    Actualización Menu

---
+++ ProyectoFinal/Assets/Comunes/Scripts/Animation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animation : MonoBehaviour {
+
+	private Animation Menu;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<UnityEngine.Animation>().wrapMode = WrapMode.Loop;
+		GetComponent<UnityEngine.Animation>().Play();
+
+
+	
+	}
+}
